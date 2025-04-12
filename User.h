88a14d70af511d51582e8e37fbcf3f5de4409a33
Timash538 <arthurmehovoy@gmail.com>
2025-04-12 20:22:37 +0300

    Добавил в User оператор сравнивания, дефолтный конструктор

--- User.h
+++ User.h
@@ -5,21 +5,22 @@ class User
 {
 public:
 
-	User(std::string _login, std::string _nickname, std::string _password) : 
-		login(_login), nickname(_nickname), password(_password) {}
-
+	User(std::string login, std::string nickname, std::string password) : 
+		_login(login), _nickname(nickname), _password(password) {}
+	User() = default;
 	std::string getNickname() const;
 	std::string getLogin() const;
 	std::string getPassword() const;
 	void setNickname(std::string&& _nickname);
 	void setLogin(std::string&& _login);
 	void setPassword(std::string&& _password);
-	bool checkPassword(std::string _password) const;
+	bool checkPassword(std::string&& _password) const;
+	bool operator==(const User &b);
 
 private:
 
-	std::string login;
-	std::string nickname;
-	std::string password;
+	std::string _login;
+	std::string _nickname;
+	std::string _password;
 
 };
