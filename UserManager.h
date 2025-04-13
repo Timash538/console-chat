6ff865ed9eb0db096989cdc60e011c323e506b98
Timash538 <arthurmehovoy@gmail.com>
2025-04-13 22:34:54 +0300

    Добавил перегруженные методы для поиска пользователей по нику и логину. Метод getCount+

--- UserManager.h
+++ UserManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "User.h"
 #include "DataManager.h"
+#include "Array.h"
 #include <memory>
 #include <string>
 
@@ -9,15 +10,19 @@ class UserManager //класс для управления пользователями
 {
 public:
 	UserManager(std::unique_ptr<DataManager<User>>&& manager) : _users(std::move(manager)) {};
+	UserManager() = default;
 	bool addUser(User&& user);
 	User getUser(int id) const;
 	User getCurrentUser() const;
 	bool findUser(User&& user, int& index);
 	bool exists(std::string&&, std::string&&);
 	bool findUserByNickname(std::string&& nickname, int& index);
+	bool findUserByNickname(std::string&& nickname);
 	bool findUserByLogin(std::string&& login, int& index);
+	bool findUserByLogin(std::string&& login);
 	bool loginUser(std::string&& login, std::string&& password);
 	void showUsers();
+	int getCount() const;
 private:
 	std::unique_ptr<DataManager<User>> _users;
 	std::unique_ptr<User> currentUser;
