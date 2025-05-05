5b33edadf6da06c6e593cccbccbf904111cbe065
Timash538 <arthurmehovoy@gmail.com>
2025-05-06 01:45:45 +0300

    Переделал Array и DataManager (Они анализа и доработок просят)

--- DataManager.h
+++ DataManager.h
@@ -1,12 +1,14 @@
 #pragma once
+#include <memory>
 
 template <typename T>
 class DataManager // Абстрактный класс, чтобы было удобно выбрать какой способ сохранения мы используем
 {
 public:
 	virtual void add(T&&) = 0;
-	virtual void deleteAt(int i) = 0;
-	virtual T get(int i) const = 0;
-	virtual int getCount() const = 0;
-	virtual bool find(T&& item, int& index) = 0;
+	virtual void deleteAt(unsigned int i) = 0;
+	virtual T& get(unsigned int i) = 0;
+	virtual unsigned int getCount() const = 0;
+	virtual ~DataManager() = default;
+	virtual std::unique_ptr<DataManager<T>>clone() const = 0;
 };
